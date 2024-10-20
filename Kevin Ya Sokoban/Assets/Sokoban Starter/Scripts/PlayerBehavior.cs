0f102a96eb80daf7634468a51728f5b4ebd45369
QueenVol <159581003+QueenVol@users.noreply.github.com>
2024-10-19 20:19:51 -0400

    Player and Sticky

--- Kevin Ya Sokoban/Assets/Sokoban Starter/Scripts/PlayerBehavior.cs
+++ Kevin Ya Sokoban/Assets/Sokoban Starter/Scripts/PlayerBehavior.cs
@@ -11,6 +11,7 @@ public class PlayerBehavior : MonoBehaviour
     public Vector2Int thePosition;
     public WallBehavior wallBehavior;
     public SmoothBehavior smoothBehavior;
+    public StickyBehavior stickyBehavior;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +20,25 @@ public class PlayerBehavior : MonoBehaviour
         playerPosition = gridObject.gridPosition;
         wallBehavior = FindObjectOfType<WallBehavior>();
         smoothBehavior = FindObjectOfType<SmoothBehavior>();
+        stickyBehavior = FindObjectOfType<StickyBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(playerPosition);
         thePosition = gridObject.gridPosition;
         if (Input.GetKeyDown(KeyCode.W))
         {
             playerPosition.y--;
+            if (stickyBehavior.moveWithPlayer)
+            {
+                stickyBehavior.stickyPosition.y--;
+            }
             if (playerPosition == wallBehavior.wallPosition)
             {
                 playerPosition = gridObject.gridPosition;
+                stickyBehavior.stickyPosition.y++;
             }
             else if (playerPosition == smoothBehavior.smoothPosition)
             {
@@ -38,15 +46,27 @@ public class PlayerBehavior : MonoBehaviour
             }
             else
             {
-                gridObject.gridPosition = playerPosition;
+                if (stickyBehavior.stickyPosition.y < 1)
+                {
+                    playerPosition = gridObject.gridPosition;
+                }
+                else
+                {
+                    gridObject.gridPosition = playerPosition;
+                }
             }
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             playerPosition.x--;
+            if (stickyBehavior.moveWithPlayer)
+            {
+                stickyBehavior.stickyPosition.x--;
+            }
             if (playerPosition == wallBehavior.wallPosition)
             {
                 playerPosition = gridObject.gridPosition;
+                stickyBehavior.stickyPosition.x++;
             }
             else if (playerPosition == smoothBehavior.smoothPosition)
             {
@@ -54,15 +74,27 @@ public class PlayerBehavior : MonoBehaviour
             }
             else
             {
-                gridObject.gridPosition = playerPosition;
+                if (stickyBehavior.stickyPosition.x < 1)
+                {
+                    playerPosition = gridObject.gridPosition;
+                }
+                else
+                {
+                    gridObject.gridPosition = playerPosition;
+                }
             }
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             playerPosition.y++;
+            if (stickyBehavior.moveWithPlayer)
+            {
+                stickyBehavior.stickyPosition.y++;
+            }
             if (playerPosition == wallBehavior.wallPosition)
             {
                 playerPosition = gridObject.gridPosition;
+                stickyBehavior.stickyPosition.y--;
             }
             else if (playerPosition == smoothBehavior.smoothPosition)
             {
@@ -70,15 +102,27 @@ public class PlayerBehavior : MonoBehaviour
             }
             else
             {
-                gridObject.gridPosition = playerPosition;
+                if (stickyBehavior.stickyPosition.y > GridMaker.reference.dimensions.y)
+                {
+                    playerPosition = gridObject.gridPosition;
+                }
+                else
+                {
+                    gridObject.gridPosition = playerPosition;
+                }
             }
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             playerPosition.x++;
+            if (stickyBehavior.moveWithPlayer)
+            {
+                stickyBehavior.stickyPosition.x++;
+            }
             if (playerPosition == wallBehavior.wallPosition)
             {
                 playerPosition = gridObject.gridPosition;
+                stickyBehavior.stickyPosition.x--;
             }
             else if (playerPosition == smoothBehavior.smoothPosition)
             {
@@ -86,7 +130,14 @@ public class PlayerBehavior : MonoBehaviour
             }
             else
             {
-                gridObject.gridPosition = playerPosition;
+                if (stickyBehavior.stickyPosition.x > GridMaker.reference.dimensions.x)
+                {
+                    playerPosition = gridObject.gridPosition;
+                }
+                else
+                {
+                    gridObject.gridPosition = playerPosition;
+                }
             }
         }
 
--- Kevin Ya Sokoban/Assets/Sokoban Starter/Scripts/StickyBehavior.cs
+++ Kevin Ya Sokoban/Assets/Sokoban Starter/Scripts/StickyBehavior.cs
@@ -6,17 +6,64 @@ public class StickyBehavior : MonoBehaviour
 {
     private GridObject gridObject;
     public Vector2Int stickyPosition;
+    private Vector2Int up;
+    private Vector2Int left;
+    private Vector2Int down;
+    private Vector2Int right;
+    public PlayerBehavior playerBehavior;
+    public bool moveWithPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         gridObject = GetComponent<GridObject>();
         stickyPosition = gridObject.gridPosition;
+        playerBehavior = FindObjectOfType<PlayerBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(stickyPosition);
+        up = stickyPosition;
+        up.y = stickyPosition.y - 1;
+        left = stickyPosition;
+        left.x = stickyPosition.x - 1;
+        down = stickyPosition;
+        down.y = stickyPosition.y + 1;
+        right = stickyPosition;
+        right.x = stickyPosition.x + 1;
+
+        if (up == playerBehavior.thePosition || left == playerBehavior.thePosition || down == playerBehavior.thePosition || right == playerBehavior.thePosition)
+        {
+            moveWithPlayer = true;
+        }
+        else
+        {
+            moveWithPlayer = false;
+        }
+
+        gridObject.gridPosition = stickyPosition;
+
+        if (gridObject.gridPosition.x < 1)
+        {
+            gridObject.gridPosition.x = 1;
+            stickyPosition = gridObject.gridPosition;
+        }
+        if (gridObject.gridPosition.x > GridMaker.reference.dimensions.x)
+        {
+            gridObject.gridPosition.x = gridObject.gridPosition.x - 1;
+            stickyPosition = gridObject.gridPosition;
+        }
+        if (gridObject.gridPosition.y < 1)
+        {
+            gridObject.gridPosition.y = 1;
+            stickyPosition = gridObject.gridPosition;
+        }
+        if (gridObject.gridPosition.y > GridMaker.reference.dimensions.y)
+        {
+            gridObject.gridPosition.y = gridObject.gridPosition.y - 1;
+            stickyPosition = gridObject.gridPosition;
+        }
     }
 }
